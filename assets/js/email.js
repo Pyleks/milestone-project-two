d0f40decdd762073fe4ae63adc36e3fe763461fa
Pyleks <aleksander.andersen@outlook.ie>
2020-01-15 20:24:43 +0000

    updated mailJS code with failure response

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,3 +1,5 @@
+// This code is partial from the mailJS tutorial in Code Institute.
+// First section only link to the elements in the template in mailJS.
 function sendMail(contactForm) {
     emailjs.init("user_qeOnqYBPzVybZ4G2BOqsg");
     var templateParams = {
@@ -6,16 +8,26 @@ function sendMail(contactForm) {
         "from_email": contactForm.emailaddress.value,
         "Blockworm_feedback": contactForm.feedback.value
     };
+    // Sends the email through mailJS
+    // Also makes the button responsive when clicked, if success the button will changed text
+    // And become green
     emailjs.send("gmail", "blockworm_feedback", templateParams)
         .then(
             function(response) {
                 console.log("SUCCESS", response);
                 $(".feedback_button").text("Thank you");
                 $(".feedback_button").removeClass("btn-secondary");
-                $(".feedback_button").addClass("red_button");
+                $(".feedback_button").addClass("white_cl");
+                $(".feedback_button").addClass("dark_green_bg");
+
             },
+            // Change the color of button to red, with "Try Again Later"
             function(error) {
                 console.log("FAILED", error);
+                $(".feedback_button").text("Try Again Later");
+                $(".feedback_button").removeClass("btn-secondary");
+                $(".feedback_button").addClass("white_cl");
+                $(".feedback_button").addClass("red_bg");
             }
         );
     return false;  // To block from loading a new page
