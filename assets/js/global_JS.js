b779c2f509e2f9b0421a1f88afdd3aca82cdd034
Pyleks <aleksander.andersen@outlook.ie>
2020-01-15 20:25:52 +0000

    Added comment to global_JS

--- assets/js/global_JS.js
+++ assets/js/global_JS.js
@@ -1,8 +1,9 @@
-// Target the navbar to create a box when hoovering
+// Target the navbar to create a green box when hoovering
 $(document).ready(function() {
     $(".nav_link_style").mouseover(function () {
         $(this).addClass("target_border")
     });
+    // When removing the mouse cursor the green box go away
     $(".nav_link_style").mouseleave(function () {
         $(this).removeClass("target_border")
     });
